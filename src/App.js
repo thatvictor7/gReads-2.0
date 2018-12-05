b62487de6e75bf34d770d0a02ab75b760b0e9386
thatvictor7 <montoya.victor7@gmail.com>
2018-12-05 14:37:25 -0700

    used a componentdid mount to pull data from backend, used props to use it on the book display file

--- src/App.js
+++ src/App.js
@@ -2,13 +2,32 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header'
 import BookDisplay from './components/BookDisplay'
+// import PropTypes from 'prop-types';
+// import { withStyles } from '@material-ui/core/styles';
+import LinearProgress from '@material-ui/core/LinearProgress';
 
 class App extends Component {
+
+    constructor(props) {
+      super(props)
+      this.state = {
+      }
+    }
+
+  componentDidMount() {
+    fetch('http://localhost:5000/gbooks')
+    .then(res => res.json())
+    .then(data => {
+      return this.setState({gbookLib: data.gBooks})
+      
+    })
+  }
+
   render() {
     return (
       <div>
       <Header />
-      <BookDisplay />
+      {this.state.gbookLib ? <BookDisplay library={this.state.gbookLib} /> : <LinearProgress />}
       </div>
     );
   }
